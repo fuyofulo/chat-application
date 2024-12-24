2398d767352fdcc5efcf9fa777b045f01e7e490f
fuyofulo <pheonixdiaz625@gmail.com>
2024-12-25 01:44:35 +0530

    chat app logix

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("hi there");
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+let userCount = 0;
+let allSockets: WebSocket[] = [];
+
+wss.on("connection", (socket) => {
+
+    allSockets.push(socket);
+
+    userCount = userCount + 1;
+    console.log("user connected #" + userCount);
+
+    // setInterval(() => {
+    //     socket.send("you shall receive this message every 5 seconds");
+    // }, 5000);
+
+    socket.on("message", (message) => {
+        console.log("message received " + message.toString());
+        for(let i=0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            s.send(message.toString() + ": sent from server");
+        }
+    })
+
+})
