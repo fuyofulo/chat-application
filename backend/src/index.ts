41ea4b52927ff6b194fb195b9209a5fd3145e892
fuyofulo <pheonixdiaz625@gmail.com>
2024-12-25 13:23:17 +0530

    backend done

---
+++ backend/src/index.ts
@@ -0,0 +1,43 @@
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+interface User {
+    socket: WebSocket;
+    room: string;
+}
+
+let allSockets: User[] = [];
+
+wss.on("connection", (socket) => {
+
+    socket.on("message", (message) => {
+        //@ts-ignore
+        const parsedMessage = JSON.parse(message);
+        if(parsedMessage.type == "join") {
+            console.log("user joined the room "+ parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
+        }
+
+        if (parsedMessage.type == "chat") {
+            console.log("user wants to chat");
+            let currentUserRoom = null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].socket == socket) {
+                    currentUserRoom = allSockets[i].room;
+                }
+            }
+
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room == currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message)
+                    console.log(parsedMessage.payload.message);
+                }
+            }
+        }
+
+    })
+})
